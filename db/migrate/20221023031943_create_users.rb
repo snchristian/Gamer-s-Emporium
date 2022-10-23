286b257456f42412eb0a8727b9a3f8fa53df2f08
Shantel <snchristian8@gmail.com>
2022-10-23 00:56:28 -0400

    setting up user attributes

--- db/migrate/20221023031943_create_users.rb
+++ db/migrate/20221023031943_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :likes, array: true
+      t.string :platforms, array: true
 
       t.timestamps
     end
