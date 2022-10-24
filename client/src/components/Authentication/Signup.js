4a1f2cc02ba59139edac4300744a8d82975b0024
Shantel <snchristian8@gmail.com>
2022-10-24 17:29:05 -0400

    add signup form

--- client/src/components/Authentication/Signup.js
+++ client/src/components/Authentication/Signup.js
@@ -1,9 +1,34 @@
-import {React} from "react"
+import {React,useState} from "react"
 
 
 function Signup() {
+    
+    const [newUserData,setNewUserData] = useState({
+        username: "",
+        email: "",
+        password: "",
+        firstname: "",
+    })
+
+    function handleChange(event) {
+        setNewUserData({  
+          ...newUserData,
+          [event.target.name]: event.target.value
+        })
+      }
+
+
   return (
-    <div>Signup</div>
+    <div>
+        <form>
+            <h2>Signup</h2>
+            <input placeholder="User Name" type={"text"} name="username" id="username" value={newUserData.username} onChange={handleChange}/>
+            <input placeholder="Email" type={"text"} name="email" id="email"  value={newUserData.email} onChange={handleChange}/>
+            <input placeholder="password" type={"text"} name="password" id="password" value={newUserData.password} onChange={handleChange}/>
+            <input type={"submit"} value="Create An Account"/>
+        </form>
+
+    </div>
   )
 }
 
