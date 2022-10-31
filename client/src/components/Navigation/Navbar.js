6580bc3990ae3905f40a751bd5149392de1f985c
Shantel <snchristian8@gmail.com>
2022-10-31 14:12:23 -0400

    adds authorization for links

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { loggedOut } from "../../features/session/SessionsSlice"
-import { useDispatch } from 'react-redux'
+import { useDispatch,useSelector } from 'react-redux'
 import { useLogOutUserMutation } from '../../App/services/gamesApi'
 
 
@@ -9,17 +9,38 @@ function Navbar() {
 
   const [deleteUser] = useLogOutUserMutation()
   const dispatch = useDispatch()
+  const loggedIn = useSelector(state => state.session.loggedIn)
 
   function handleLoggedOut() {
     deleteUser().unwrap().then(dispatch(loggedOut()))
   }
 
-  return (
-    <div>
+
+  function unauthorizedUserLinks(){
+    return(
+      <div>
       <Link to="/Signup">Signup</Link>
       <Link to="/login">Login</Link>
-      <Link to="/">home</Link>
+      <Link to="/">Home</Link>
+      </div>
+    )
+  }
+
+  function authorizedUserLinks(){
+    return(
+       <div>
+      <Link to="/games">Games</Link>
+      <Link to="/my-games">My Games</Link>
       <button onClick={handleLoggedOut}>LogOut</button>
+    </div>
+    )
+   
+    
+  }
+
+  return (
+    <div>
+      {loggedIn ? authorizedUserLinks() : unauthorizedUserLinks()}
     </div>
   )
 }
