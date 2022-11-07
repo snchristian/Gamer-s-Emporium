ca84656e6cb3fb943fc55e0d85d5dd47e3cc81e8
Shantel <snchristian8@gmail.com>
2022-11-07 18:29:51 -0500

    setting up games data

--- client/src/components/content/Games/GamesList.js
+++ client/src/components/content/Games/GamesList.js
@@ -1,22 +1,48 @@
 import React from 'react'
 import { useFetchGamesQuery } from '../../../App/services/gamesApi'
-import GameCard from './GameCard'
+import CardSlider from './CardSlider'
+import { useSelector } from 'react-redux'
 
 function GamesList() {
-    
-    const {data=[]} = useFetchGamesQuery()
 
-    const displayGames = data.map(game => (
-        <GameCard key={game.id} game={game}/>
-    ))
+  const { data = [], isFetching } = useFetchGamesQuery()
 
+  const currentUser = useSelector(state => state.session.currentUser)
+
+  function getGamesMatchingUserLikes() {
+    if (!isFetching) {
+
+      const filitedArray = data.filter((data2) => data2.genres.some(genre => {
+        return currentUser.likes.some((like) => {
+          return like === genre
+        })
+
+      }))
+
+      const fil = filitedArray.filter(item => item.platforms.some(platform => {
+        return currentUser.platforms.some(plat => {
+          return plat === platform
+        })
+      }))
+      return fil.slice(0, 6)
+    }
+  }
+
+  function getGenreMatch(genre) {
+    const genreMatches = data.filter(data => data.genres.includes(genre))
+    return genreMatches.slice(0, 6)
+  }
+
+  if (isFetching) return <div>Loading...</div>
 
   return (
-    <main>
-      <div>GamesList</div> 
-      <div>{displayGames}</div> 
-    </main>
-    
+    <div>
+      <CardSlider title="For You" data={getGamesMatchingUserLikes()} />
+      <CardSlider title="Action" data={getGenreMatch("Action")} />
+      <CardSlider title="RPG" data={getGenreMatch("RPG")} />
+      <CardSlider title="Shooter" data={getGenreMatch("Shooter")} />
+
+    </div>
   )
 }
 
